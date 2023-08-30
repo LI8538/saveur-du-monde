3fe2a90ff4882e9c530f9fc10b2904cb0d5d6696
Gitubeuse92 <amanda_v_f@hotmail.com>
2023-08-30 10:20:00 +0200

    ✅ update Home avec avis clients

---
+++ .history/src/Controller/HomeController_20230830101643.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ReviewRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(ReviewRepository $reviewRepository,PaginatorInterface $paginator, Request $request,): Response
+    {
+        $data = $reviewRepository->findBy([], ['datePublication' => 'DESC']);
+
+        $pagination = $paginator->paginate(
+        $data, // Requête contenant les données à paginer
+
+        $request->query->getInt('page', 1), // Numéro de la page en cours, 1 par défaut
+        // Nombre de résultats par page
+    );
+
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'reviews' => $pagination
+        ]);
+    }
+}
