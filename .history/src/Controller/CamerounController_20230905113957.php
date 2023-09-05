a6ded6ffa3a98f7f488be8a87fa1e5f402e72066
Gitubeuse92 <amanda_v_f@hotmail.com>
2023-09-05 11:43:27 +0200

    👷 update findProductsByCategoryAndTypesurCameroun

---
+++ .history/src/Controller/CamerounController_20230905113957.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CamerounController extends AbstractController
+{
+    #[Route('/cameroun', name: 'app_cameroun')]
+    public function index(): Response
+    {
+        return $this->render('cameroun/index.html.twig', [
+            'controller_name' => 'CamerounController',
+        ]);
+    }
+}
+
+class BresilController extends AbstractController
+{
+    #[Route('/bresil', name: 'app_bresil', methods: ['GET'])]
+    public function index(ProductRepository $productRepository): Response
+    {
+        $entrees = $productRepository->findProductsByCategoryAndType('bresil', 'Entrée');
+        $plats = $productRepository->findProductsByCategoryAndType('bresil', 'Plat');
+        $desserts = $productRepository->findProductsByCategoryAndType('bresil', 'Dessert');
+    
+        return $this->render('bresil/index.html.twig', [
+            'entrees' => $entrees,
+            'plats' => $plats,
+            'desserts' => $desserts,
+        ]);
+
+}}
