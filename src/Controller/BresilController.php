75ada410ad6d3388f17bbef579ddb709e324fb58
Gitubeuse92 <amanda_v_f@hotmail.com>
2023-09-05 11:38:40 +0200

    👷 update findProductsByCategoryAndType sur Bresil

--- src/Controller/BresilController.php
+++ src/Controller/BresilController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BresilController extends AbstractController
 {
-    #[Route('/bresil', name: 'app_bresil')]
-    public function index(): Response
+    #[Route('/bresil', name: 'app_bresil', methods: ['GET'])]
+    public function index(ProductRepository $productRepository): Response
     {
+        $entrees = $productRepository->findProductsByCategoryAndType('bresil', 'Entrée');
+        $plats = $productRepository->findProductsByCategoryAndType('bresil', 'Plat');
+        $desserts = $productRepository->findProductsByCategoryAndType('bresil', 'Dessert');
+    
         return $this->render('bresil/index.html.twig', [
-            'controller_name' => 'BresilController',
+            'entrees' => $entrees,
+            'plats' => $plats,
+            'desserts' => $desserts,
         ]);
-    }
-}
+
+}}
