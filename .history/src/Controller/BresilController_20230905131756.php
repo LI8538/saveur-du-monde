cdc6ec3dde3ffe316ada1aaddc3fa755eccee91f
Gitubeuse92 <amanda_v_f@hotmail.com>
2023-09-05 14:43:41 +0200

    💄 update design boutton panier d'achat partout

---
+++ .history/src/Controller/BresilController_20230905131756.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ReviewRepository;
+use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class BresilController extends AbstractController
+{
+    #[Route('/bresil', name: 'app_bresil', methods: ['GET'])]
+    public function index(ReviewRepository $reviewRepository, ProductRepository $productRepository): Response
+    {
+        $reviewRepositoryData = $reviewRepository->findAll();
+        $entrees = $productRepository->findProductsByCategoryAndType('bresil', 'Entrée');
+        $plats = $productRepository->findProductsByCategoryAndType('bresil', 'Plat');
+        $desserts = $productRepository->findProductsByCategoryAndType('bresil', 'Dessert');
+    
+        return $this->render('bresil/index.html.twig', [
+            'entrees' => $entrees,
+            'plats' => $plats,
+            'desserts' => $desserts,
+            'reviews' => $desserts,
+        ]);
+
+}}
