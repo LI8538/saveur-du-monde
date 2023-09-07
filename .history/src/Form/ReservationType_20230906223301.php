b45d78b57baafd1636c4c9e22d621a941bdc47e0
Gitubeuse92 <amanda_v_f@hotmail.com>
2023-09-07 05:06:53 +0200

    ✅ update form reservation

--- .history/src/Form/ReservationType_20230906223301.php
+++ .history/src/Form/ReservationType_20230906223301.php
@@ -2,6 +2,7 @@
 
 namespace App\Form;
 
+use App\Entity\Reservation;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -48,7 +49,7 @@ class ReservationType extends AbstractType
                     ])
                 ]
             ])
-            ->add('date', DateType::class)
+            ->add('date',)
             ->add('numberPerson')
             ->add('message', TextareaType::class, [
                 'required' => true,
